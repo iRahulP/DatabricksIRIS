9f1ed989a048a570fd6ad0c1f15ba230de065c86
rahul patil <rahulpatil.m30@gmail.com>
2021-03-28 00:01:28 +0530

    updated model

--- train.py
+++ train.py
@@ -18,26 +18,22 @@ from sklearn.tree import DecisionTreeClassifier #for using Decision Tree Algoith
 
 
 if __name__ == "__main__":
+  iris_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "iris.csv")
+  data = pd.read_csv(iris_path)
+  train, test = train_test_split(data, test_size = 0.3)
+  # in this our main data is split into train and test
+  # the attribute test_size=0.3 splits the data into 70% and 30% ratio. train=70% and test=30%
+  train_X = train[['sepal_length','sepal_width','petal_length','petal_width']]# taking the training data features
+  train_y=train.species# output of our training data
+  test_X= test[['sepal_length','sepal_width','petal_length','petal_width']] # taking test data features
+  test_y =test.species   #output value of test data
+      
   with mlflow.start_run():
-    iris_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "iris.csv")
-    data = pd.read_csv(iris_path)
-
-    train, test = train_test_split(data, test_size = 0.3)# in this our main data is split into train and test
-    # the attribute test_size=0.3 splits the data into 70% and 30% ratio. train=70% and test=30%
-    #print(train.shape)
-    #print(test.shape)
-
-    train_X = train[['sepal_length','sepal_width','petal_length','petal_width']]# taking the training data features
-    train_y=train.species# output of our training data
-    test_X= test[['sepal_length','sepal_width','petal_length','petal_width']] # taking test data features
-    test_y =test.species   #output value of test data
     model = svm.SVC() #select the algorithm
     model.fit(train_X,train_y) # we train the algorithm with the training data and the training output
     prediction=model.predict(test_X) #now we pass the testing data to the trained algorithm
     accuracy = metrics.accuracy_score(prediction,test_y)
-    print(accuracy)
+    print("Accuracy :: "accuracy)
     
     mlflow.log_metric("accuracy", accuracy)
     mlflow.sklearn.log_model(model, "model")
-    modelpath = "/dbfs/mlflow/iris/model-%s-%f" % ("svm", 1)
-    mlflow.sklearn.save_model(model, modelpath)
